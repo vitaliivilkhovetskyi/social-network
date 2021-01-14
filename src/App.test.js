f9dc97ec4c261d640a094d5993821232a76db91c
vitaliivilkhovetskyi <vilkhovetskyivitalii@users.noreply.github.com>
2021-01-14 11:52:46 +0200

    Updates

--- src/App.test.js
+++
@@ -1,9 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import SamuraiJSApp from './App';
-import React from 'react';
-
-test('renders learn react link', () => {
-  const linkElement = screen.getByText(/learn react/i);
-  render(<SamuraiJSApp />);
-  expect(linkElement).toBeInTheDocument();
-});
